68340e8aa160dfb52a7b423927730075ef596cae
AaronLin <1546848781@qq.com>
2021-02-19 17:47:04 +0800

    11 内部类

---
+++ src/main/java/_11InnerClasses/_01Parcel1/Parcel1.java
@@ -0,0 +1,39 @@
+package _11InnerClasses._01Parcel1;
+
+/**
+ * @author Aaron
+ * @date 2021-02-19 17:41
+ */
+public class Parcel1 {
+    class Contents {
+        private int i = 11;
+
+        public int value() {
+            return i;
+        }
+    }
+
+    class Destination {
+        private String label;
+
+        Destination(String whereTo) {
+            label = whereTo;
+        }
+
+        String readLabel() {
+            return label;
+        }
+    }
+
+    public void ship(String dest) {
+        Contents c = new Contents();
+        Destination d = new Destination(dest);
+        System.out.println(d.readLabel());
+    }
+
+    public static void main(String[] args) {
+        Parcel1 p = new Parcel1();
+        p.ship("Tasmania");
+    }
+    
+}
